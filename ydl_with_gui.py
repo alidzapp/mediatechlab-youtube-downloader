9312f7dce1f91d62bb4e0cc7da29138f5bd6110d
DömePapa <demeter.333@gmail.com>
2015-11-22 11:45:10 +0100

    check button is working, but grid has a little bug

--- ydl_with_gui.py
+++ ydl_with_gui.py
@@ -1,29 +1,90 @@
+#!/usr/bin/env python3
+
 from tkinter import *
 from tkinter import ttk
+# to show the video pic
+from urllib.request import urlopen
+from PIL import Image, ImageTk
+import io
+# to download from youtube
+import pafy
+
+
+#----------------------------------------------------------------------------------------
+def url_validation(url):
+    youtube_regex = (
+        r'(https?://)?(www\.)?'
+        '(youtube|youtu|youtube-nocookie)\.(com|be)/'
+        '(watch\?v=|embed/|v/|.+\?v=)?([^&=%\?]{11})')
+
+    youtube_regex_match = re.match(youtube_regex, url)
+    if youtube_regex_match:
+        return youtube_regex_match.group(6)
+
+    raise Exception("URL validation failed!!!")
 
 def check(*args):
     try:
-        print("Check command")
-    except ValueError:
-        pass
+        #--------------------------------------------------------------------------------
+        # show thumbnail
+        #--------------------------------------------------------------------------------
+        pic_url = "http://img.youtube.com/vi/" + url_validation(url.get())   + "/0.jpg"
+        # open the web page picture and read it into a memory stream
+        # and convert to an image Tkinter can handle
+        dl_pic = urlopen(pic_url)
+        # create an image file object
+        my_picture = io.BytesIO(dl_pic.read())
+        # use PIL to open image formats like .jpg  .png  .gif  etc.
+        pil_img = Image.open(my_picture)
+        pil_img = pil_img.resize((240,180))
+        # convert to an image Tkinter can use
+        tk_img  = ImageTk.PhotoImage(pil_img)
+        # put the image on a typical widget
+        video_pic = ttk.Label(left, image=tk_img)
+        video_pic.grid(column=0, row=0, sticky=W)
 
+        #--------------------------------------------------------------------------------
+        # change title + durration + streams
+        #--------------------------------------------------------------------------------
+        try:
+            video = pafy.new(url.get())
+            title = video.title
+            right_name = ttk.Label(right, text=title[:35])            
+            right_name.grid(column=0, row=0, sticky=W)
+            right_duration = ttk.Label(right, text=video.duration)            
+            right_duration.grid(column=0, row=1, sticky=W)
+
+            right_vformats = video.streams
+            right_vcombo = ttk.Combobox(right, values=right_vformats)
+            right_vcombo.grid(column=0, row=3, sticky=W, pady=15)
+
+            right_aformats = video.audiostreams
+            right_acombo = ttk.Combobox(right, values=right_aformats)
+            right_acombo.grid(column=0, row=5, sticky=W, pady=15)
+        except Exception as e:
+            print("change title + durration + streams")
+            print(e)
+
+        #--------------------------------------------------------------------------------
+        # refresh gui
+        #--------------------------------------------------------------------------------
+        root.mainloop()
+    except Exception as e:
+        print(e)
+
+#----------------------------------------------------------------------------------------
 def download(*args):
     try:
         print("Download command")
-    except ValueError:
-        pass
-    
-def dlg(*args):
-    try:
-        print("Dlg command")
-        messengerbox.showinfo('About', 'Home Work\nMedia Technology Laboratory 2.\nCreated by Peter Demeter\n2015')
     except ValueError:
         pass
 
 root = Tk()
 root.title("YouTube Downloader")
 
-#FRAMES
+#----------------------------------------------------------------------------------------
+# FRAMES
+#----------------------------------------------------------------------------------------
 mainframe = ttk.Frame(root, padding="20 20 20 20")
 top    = ttk.Frame(mainframe, borderwidth=5)
 left   = ttk.Frame(mainframe, borderwidth=5)
@@ -35,47 +96,54 @@ left.grid(     column=0, row=1, sticky=W)
 right.grid(    column=1, row=1, sticky=E)
 bottom.grid(   column=0, row=2, sticky=(W,S), columnspan=2)
 
-feet = StringVar()
-meters = StringVar()
-
-#TOP
-top_text  = ttk.Label(top, text="Enter video URL")
-top_entry = ttk.Entry(top, textvariable=feet, width=43)
+#----------------------------------------------------------------------------------------
+# TOP
+#----------------------------------------------------------------------------------------
+url = StringVar()
+url.set("https://www.youtube.com/watch?v=cJEjQxuc5yk") # ONLY FOR TEST!
+top_text  = ttk.Label(top,  text="Enter video URL")
+top_entry = ttk.Entry(top,  textvariable=url, width=43)
 top_check = ttk.Button(top, text="Check", command=check)
 top_text.grid( column=0, row=0, sticky=W)
 top_entry.grid(column=0, row=1, sticky=W)
 top_check.grid(column=1, row=1, sticky=W, padx=15)
 
-#LEFT
-dummy_pic = Canvas(left, width=240, height=160, bg='black')
+#----------------------------------------------------------------------------------------
+# LEFT
+#----------------------------------------------------------------------------------------
+dummy_pic = Canvas(left, width=240, height=180, bg='black')
 dummy_pic.grid(column=0, row=0, sticky=W)
 
-#RIGHT
-video_name     = "dummy video name"
-right_vformats = StringVar()
-right_aformats = StringVar()
-right_name   = ttk.Label(right, text=video_name)
+#----------------------------------------------------------------------------------------
+# RIGHT
+#----------------------------------------------------------------------------------------
+right_title    = ttk.Label(right, text="")
+right_duration = ttk.Label(right, text="")
 right_video  = ttk.Label(right, text="VIDEO format")
-right_vcombo = ttk.Combobox(right, textvariable=right_vformats)
+right_vcombo = ttk.Combobox(right, textvariable="")
 right_audio  = ttk.Label(right, text="AUDIO format")
-right_acombo = ttk.Combobox(right, textvariable=right_aformats)
-right_name.grid(  column=0, row=0, sticky=W)
-right_video.grid( column=0, row=1, sticky=W, pady=15)
-right_vcombo.grid(column=0, row=2, sticky=W, pady=15)
-right_audio.grid( column=0, row=3, sticky=W, pady=15)
-right_acombo.grid(column=0, row=4, sticky=W, pady=15)
+right_acombo = ttk.Combobox(right, textvariable="")
+right_title.grid(   column=0, row=0, sticky=W)
+right_duration.grid(column=0, row=1, sticky=W)
+right_video.grid(   column=0, row=2, sticky=W, pady=10)
+right_vcombo.grid(  column=0, row=3, sticky=W, pady=15)
+right_audio.grid(   column=0, row=4, sticky=W, pady=10)
+right_acombo.grid(  column=0, row=5, sticky=W, pady=15)
 
-#BOTTOM -----------
+#----------------------------------------------------------------------------------------
+# BOTTOM
+#----------------------------------------------------------------------------------------
 bottom_path     = "C:\dummy\demo"
 bottom_text     = ttk.Label(bottom, text="SAVE TO:")
 bottom_plabel   = ttk.Label(bottom, text=bottom_path)
-#bottom_dlgbtn	= ttk.Button(bottom, text="Browse", command=dlg)
 bottom_download = ttk.Button(bottom, text="Download", command=download)
 bottom_progress = ttk.Progressbar(bottom, orient=HORIZONTAL, length=350, mode='determinate')
 bottom_text.grid(    column=0, row=0, sticky=W)
-bottom_plabel.grid(	 column=0, row=1, sticky=W)
-#bottom_dlgbtn(       column=1, row=1, pady=15)
+bottom_plabel.grid(  column=0, row=1, sticky=W)
 bottom_download.grid(column=2, row=3, sticky=E, padx=15)
 bottom_progress.grid(column=0, row=3, columnspan=2, pady=15)
 
+#----------------------------------------------------------------------------------------
+# draw gui
+#----------------------------------------------------------------------------------------
 root.mainloop()
